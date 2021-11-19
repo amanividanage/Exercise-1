29d80895e56ae4737c6b258c2ae3df5e919428d0
amanividanage <90959983+amanividanage@users.noreply.github.com>
2021-11-19 20:27:16 +0530

    Add files via upload

---
+++ Exercise 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	 double height, width, area;
+	 printf("Enter the height: ");
+	  scanf("%lf", &height);
+	 printf("Enter the width: ");
+	 scanf("%lf ", &width);
+	 area = height * width;
+	 printf("The area is : %lf", area);
+	
+	 
+}
